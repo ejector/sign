d0ce3c5e4bcfa6626c6622e14c49fb29a877ac58
deboloed <deboloed@gmail.com>
2022-09-04 14:38:47 +0300

    added SignReader and Randomizer classes to tests

---
+++ tests/randomizer.h
@@ -0,0 +1,27 @@
+#ifndef RANDOMIZER_H
+#define RANDOMIZER_H
+
+#include <limits>
+#include <random>
+
+template<typename T>
+class Randomizer final
+{
+public:
+    Randomizer(T min, T max = std::numeric_limits<T>::max())
+        : _generator(_random_device())
+        , _distrib(min, max)
+    {
+
+    }
+    int random()
+    {
+        return _distrib(_generator);
+    }
+protected:
+    std::random_device _random_device;
+    std::mt19937 _generator;
+    std::uniform_int_distribution<> _distrib;
+};
+
+#endif // RANDOMIZER_H
