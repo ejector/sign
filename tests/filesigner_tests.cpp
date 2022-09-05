0611998afc7f06ec11b1999525867a3ded8fbdb9
deboloed <deboloed@gmail.com>
2022-09-06 01:40:28 +0300

    moved generate test data to separate functions

--- tests/filesigner_tests.cpp
+++ tests/filesigner_tests.cpp
@@ -1,25 +1,117 @@
 #include <fstream>
+#include <filesystem>
 
 #include <gtest/gtest.h>
 
 #include "randomizer.h"
-#include "signreader.h"
 
 #include "../src/hashrot13.h"
 #include "../src/filesigner.h"
+#include "../src/signreader.h"
 
 class FileSignerTest : public ::testing::Test
 {
 protected:
-    void SetUp() override
-    {
+    using TestDataType  = std::tuple<std::string, SignReader::HashArray>;
 
+    void TearDown() override
+    {
+        std::filesystem::remove(kInputFileName);
+        std::filesystem::remove(kOutpuFileName);
     }
+
+    TestDataType GenTestDataForFileSizeEqualZero()
+    {
+        std::ofstream(kInputFileName, std::ios::binary | std::ios::trunc);
+        return {kInputFileName, {}};
+    }
+
+    TestDataType GenTestDataForFileSizeEqualBlockSize()
+    {
+        std::array<char, kBlockSize> block;
+        Randomizer<char> randomizer(0);
+        std::for_each(std::begin(block), std::end(block), [&](auto&& item) {
+            item = randomizer.random();
+        });
+
+        auto hash = HashRot13<SignReader::HashType>(std::begin(block), std::end(block));
+        SignReader::HashArray expected_signature(1, hash);
+
+        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
+        std::copy(std::begin(block), std::end(block), std::ostream_iterator<char>(file));
+
+        return {kInputFileName, expected_signature};
+    }
+
+    TestDataType GenTestDataForFileSizeLessBlockSize()
+    {
+        constexpr size_t data_size  = kBlockSize / 2;
+
+        std::array<char, data_size> block;
+        Randomizer<char> randomizer(0);
+        std::for_each(std::begin(block), std::end(block), [&](auto&& item) {
+            item = randomizer.random();
+        });
+
+        auto hash = HashRot13<SignReader::HashType>(std::begin(block), std::end(block));
+        SignReader::HashArray expected_signature(1, hash);
+
+        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
+        std::copy(std::begin(block), std::end(block), std::ostream_iterator<char>(file));
+
+        return {kInputFileName, expected_signature};
+    }
+
+    TestDataType GenTestDataForFileSizeGreaterBlockSize()
+    {
+        constexpr size_t data_size  = kBlockSize + kBlockSize / 2;
+
+        std::array<char, data_size> data;
+        Randomizer<char> randomizer(0);
+        std::for_each(std::begin(data), std::end(data), [&](auto&& item) {
+            item = randomizer.random();
+        });
+
+        SignReader::HashArray expected_signature(2, 0);
+
+        expected_signature.at(0) = HashRot13<SignReader::HashType>(std::begin(data), std::begin(data) + kBlockSize);
+        expected_signature.at(1) = HashRot13<SignReader::HashType>(std::begin(data) + kBlockSize, std::end(data));
+
+        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
+        std::copy(std::begin(data), std::end(data), std::ostream_iterator<char>(file));
+
+        return {kInputFileName, expected_signature};
+    }
+
+    TestDataType GenTestDataForFileHasManyBlocks()
+    {
+        constexpr size_t data_size  = 1000 * kBlockSize + kBlockSize - 1;
+
+        std::vector<char> data(data_size, 0);
+        Randomizer<char> randomizer(0);
+        std::for_each(std::begin(data), std::end(data), [&](auto&& item) {
+            item = randomizer.random();
+        });
+
+        auto block_count = data_size / kBlockSize + (data_size % kBlockSize != 0 ? 1 : 0);
+        SignReader::HashArray expected_signature;
+        for (size_t i = 0; i < block_count; ++i) {
+            auto begin = std::begin(data) + i * kBlockSize;
+            auto end = std::distance(begin, std::end(data)) < kBlockSize ? std::end(data) : begin + kBlockSize;
+            expected_signature.push_back(HashRot13<SignReader::HashType>(begin, end));
+        }
+
+        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
+        std::copy(std::begin(data), std::end(data), std::ostream_iterator<char>(file));
+
+        return {kInputFileName, expected_signature};
+    }
+
+    static constexpr size_t kBlockSize = 1 * 1024 * 1024;
 
     const std::string kInputFileName = "input.bin";
     const std::string kOutpuFileName = "output.sign";
 };
-
 
 TEST_F(FileSignerTest, GenerateSignNegative)
 {
@@ -29,18 +121,20 @@ TEST_F(FileSignerTest, GenerateSignNegative)
     ASSERT_NE("", file_signer.GetErrorString());
 }
 
+TEST_F(FileSignerTest, GenerateSignWhenBlockSizeEqualZero)
+{
+    FileSigner file_signer;
+
+    ASSERT_EQ(false, file_signer.GenerateSign("", "", 0));
+    ASSERT_NE("", file_signer.GetErrorString());
+}
+
 TEST_F(FileSignerTest, GenerateSignWhenFileSizeEqualZero)
 {
-    constexpr size_t block_size = 1 * 1024 * 1024;
-
-    std::vector<unsigned int> expected_signature;
-
-    {
-        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
-    }
+    auto&& [input_file_name, expected_signature] = GenTestDataForFileSizeEqualZero();
 
     FileSigner file_signer;
-    ASSERT_EQ(true, file_signer.GenerateSign(kInputFileName, kOutpuFileName, block_size));
+    ASSERT_EQ(true, file_signer.GenerateSign(input_file_name, kOutpuFileName, kBlockSize));
 
     auto actual_signature = SignReader::Read(kOutpuFileName);
     ASSERT_EQ(expected_signature, actual_signature);
@@ -48,24 +142,10 @@ TEST_F(FileSignerTest, GenerateSignWhenFileSizeEqualZero)
 
 TEST_F(FileSignerTest, GenerateSignWhenFileSizeEqualBlockSize)
 {
-    constexpr size_t block_size = 1 * 1024 * 1024;
-
-    std::array<char, block_size> block;
-    Randomizer<char> randomizer(0);
-    std::for_each(std::begin(block), std::end(block), [&](auto&& item) {
-        item = randomizer.random();
-    });
-
-    auto hash = HashRot13(std::begin(block), std::end(block));
-    std::vector<unsigned int> expected_signature(1, hash);
-
-    {
-        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
-        std::copy(std::begin(block), std::end(block), std::ostream_iterator<char>(file));
-    }
+    auto&& [input_file_name, expected_signature] = GenTestDataForFileSizeEqualBlockSize();
 
     FileSigner file_signer;
-    ASSERT_EQ(true, file_signer.GenerateSign(kInputFileName, kOutpuFileName, block_size));
+    ASSERT_EQ(true, file_signer.GenerateSign(input_file_name, kOutpuFileName, kBlockSize));
 
     auto actual_signature = SignReader::Read(kOutpuFileName);
     ASSERT_EQ(expected_signature, actual_signature);
@@ -73,28 +153,10 @@ TEST_F(FileSignerTest, GenerateSignWhenFileSizeEqualBlockSize)
 
 TEST_F(FileSignerTest, GenerateSignWhenFileSizeLessBlockSize)
 {
-    constexpr size_t block_size = 1 * 1024 * 1024;
-    constexpr size_t data_size  = block_size / 2;
-
-    std::array<char, data_size> block;
-    Randomizer<char> randomizer(0);
-    std::for_each(std::begin(block), std::end(block), [&](auto&& item) {
-        item = randomizer.random();
-    });
-
-    auto hash = HashRot13(std::begin(block), std::end(block));
-    std::vector<unsigned int> expected_signature(1, hash);
-
-    {
-        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
-        std::copy(std::begin(block), std::end(block), std::ostream_iterator<char>(file));
-    }
+    auto&& [input_file_name, expected_signature] = GenTestDataForFileSizeLessBlockSize();
 
     FileSigner file_signer;
-    ASSERT_EQ(true, file_signer.GenerateSign(kInputFileName, kOutpuFileName, block_size));
-
-    std::ifstream file_sign(kOutpuFileName, std::ios::binary);
-    ASSERT_EQ(true, file_sign.is_open());
+    ASSERT_EQ(true, file_signer.GenerateSign(input_file_name, kOutpuFileName, kBlockSize));
 
     auto actual_signature = SignReader::Read(kOutpuFileName);
     ASSERT_EQ(expected_signature, actual_signature);
@@ -102,58 +164,21 @@ TEST_F(FileSignerTest, GenerateSignWhenFileSizeLessBlockSize)
 
 TEST_F(FileSignerTest, GenerateSignWhenFileSizeGreaterBlockSize)
 {
-    constexpr size_t block_size = 1 * 1024 * 1024;
-    constexpr size_t data_size  = block_size + block_size / 2;
-
-    std::array<char, data_size> data;
-    Randomizer<char> randomizer(0);
-    std::for_each(std::begin(data), std::end(data), [&](auto&& item) {
-        item = randomizer.random();
-    });
-
-    std::vector<unsigned int> expected_signature(2, 0);
-
-    expected_signature.at(0) = HashRot13(std::begin(data), std::begin(data) + block_size);
-    expected_signature.at(1) = HashRot13(std::begin(data) + block_size, std::end(data));
-
-    {
-        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
-        std::copy(std::begin(data), std::end(data), std::ostream_iterator<char>(file));
-    }
+    auto&& [input_file_name, expected_signature] = GenTestDataForFileSizeGreaterBlockSize();
 
     FileSigner file_signer;
-    ASSERT_EQ(true, file_signer.GenerateSign(kInputFileName, kOutpuFileName, block_size));
+    ASSERT_EQ(true, file_signer.GenerateSign(input_file_name, kOutpuFileName, kBlockSize));
 
     auto actual_signature = SignReader::Read(kOutpuFileName);
     ASSERT_EQ(expected_signature, actual_signature);
 }
 
-TEST_F(FileSignerTest, GenerateSignWhenFileSizeManyBlockSize)
+TEST_F(FileSignerTest, GenerateSignWhenFileHasManyBlocks)
 {
-    constexpr size_t block_size = 1 * 1024 * 1024;
-    constexpr size_t data_size  = 1000 * block_size + block_size - 1;
-
-    std::vector<char> data(data_size, 0);
-    Randomizer<char> randomizer(0);
-    std::for_each(std::begin(data), std::end(data), [&](auto&& item) {
-        item = randomizer.random();
-    });
-
-    auto block_count = data_size / block_size + (data_size % block_size != 0 ? 1 : 0);
-    std::vector<unsigned int> expected_signature;
-    for (size_t i = 0; i < block_count; ++i) {
-        auto begin = std::begin(data) + i * block_size;
-        auto end = std::distance(begin, std::end(data)) < block_size ? std::end(data) : begin + block_size;
-        expected_signature.push_back(HashRot13(begin, end));
-    }
-
-    {
-        std::ofstream file(kInputFileName, std::ios::binary | std::ios::trunc);
-        std::copy(std::begin(data), std::end(data), std::ostream_iterator<char>(file));
-    }
+    auto&& [input_file_name, expected_signature] = GenTestDataForFileHasManyBlocks();
 
     FileSigner file_signer;
-    ASSERT_EQ(true, file_signer.GenerateSign(kInputFileName, kOutpuFileName, block_size));
+    ASSERT_EQ(true, file_signer.GenerateSign(input_file_name, kOutpuFileName, kBlockSize));
 
     auto actual_signature = SignReader::Read(kOutpuFileName);
     ASSERT_EQ(expected_signature, actual_signature);
