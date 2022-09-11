5137767511dd0e1c71f4b1a89d4b4a213ed842b4
deboloed <deboloed@gmail.com>
2022-09-11 22:20:40 +0300

    added TaskPool

---
+++ src/taskpool.h
@@ -0,0 +1,37 @@
+#ifndef TASKPOOL_H
+#define TASKPOOL_H
+
+#include <functional>
+#include <future>
+#include <queue>
+
+#include "threadpool.h"
+
+class TaskPool final
+{
+public:
+    TaskPool()
+        : TaskPool(ThreadPool::instance().MaximumThreadCount())
+    {
+    }
+    TaskPool(size_t count)
+        : count_(count)
+    {
+    }
+    void WaitIfFullAndExec(std::function<void()> func)
+    {
+        if (queue_.size() >= count_) {
+            auto f = std::move(queue_.front());
+            if (f.valid()) {
+                f.get();
+            }
+            queue_.pop();
+        }
+        queue_.emplace(ThreadPool::Instance().Async(func));
+    }
+protected:
+    std::queue<std::future<void>> queue_;
+    size_t count_;
+};
+
+#endif // TASKPOOL_H
