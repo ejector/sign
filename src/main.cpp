485eaf9217c68d3b22772a3b68362faf8561ff6d
deboloed <deboloed@gmail.com>
2022-09-28 23:29:06 +0300

    added const

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char** argv)
     try {
         CommandLineApp app(argc, argv);
         return app.Exec();
-    } catch (std::exception& e) {
+    } catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
     } catch (...) {
-        std::cout << "An errror occured" << std::endl;
+        std::cout << "An errror occured. Exit" << std::endl;
     }
     return CommandLineApp::kExitWithError;
 }
